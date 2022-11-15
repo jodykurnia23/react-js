b248b1ad174205120017696d38b2706088a17d69
jody_kurnia <jodykurnia2@gmail.com>
2022-11-15 17:04:15 +0700

    Class Componenet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import komponen from "./materi/komponen";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br/>
-          Oke udah ditambahkan, mantapppppp
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <komponen />
     </div>
   );
 }
---
+++ src/materi/komponen/pembahasan/ClassComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class ClassComponent extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Komponen ini dibuat mengunakan ClassComponent</h1>
+                <h2>Halo Dunia!</h2>
+            </div>
+        )
+    }
+};
